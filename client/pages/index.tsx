744bfe8d7902cebabed550ab60673c10eb2aa507
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-29 16:14:09 +0900

    fix: home screen

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,37 +1,37 @@
 //minimize.com/
 
-import React, { useEffect, useState, type ReactElement} from "react";
+import React, { useEffect, useState, type ReactElement } from "react";
 // import { type NextPage } from "next";
-import {
-  Box,
-  Grid,
-  Typography,
-} from "@mui/material";
 import SignIn from "../components/SignIn";
 import Register from "../components/Register";
+// import heroPhoto1 from "../public/heroPhoto1.jpg";
+// import Image from 'next/image';
 
 const HomePage = (): ReactElement => {
   const [toggle, setToggle] = useState<Boolean>(true);
-  const handleClose = () => { setToggle(false) }
-  const handleOpen = () => { setToggle(true) }
+  const handleClose = () => {
+    setToggle(false);
+  };
+  const handleOpen = () => {
+    setToggle(true);
+  };
 
   return (
-    <Grid container spacing={2} id="wrapper__homePage" className="text-center">
-    <Grid item>
-      <Box>
-        <Typography variant="h3" align="center" gutterBottom> Curate, Sort, Free your Life.</Typography>
-      </Box>
-    </Grid>
+    <div className="grid w-screen h-screen grid-cols-2 place-items-center">
+      <div className="self-center">
+        {/* <Image src={heroPhoto1} alt="" /> */}
+        <h1 className="text-3xl"> Curate, Sort, Free your Life.</h1>
+      </div>
 
-    <Grid item>
- <div className="flex flex-col items-center justify-center my-5 bg-white rounded-md shadow-md">
-      {toggle? <SignIn handleClose={handleClose}/>:
-      <Register handleOpen={handleOpen}/>}
-   </div>
-      </Grid>
-      </Grid>
-     
+      <div className="flex flex-col items-center h-auto my-5 bg-white rounded-md shadow-md justify-right w-80">
+        {toggle ? (
+          <SignIn handleClose={handleClose} />
+        ) : (
+          <Register handleOpen={handleOpen} />
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default HomePage;
